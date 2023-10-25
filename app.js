b8e5338acd634c09599162d3c025fbf7ff4cbaf3
HimanshuJ-dev <63807131+HimanshuJ-dev@users.noreply.github.com>
2023-10-26 04:20:12 +0530

    directly put user and pass in mongodb url

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.q1sgt3q.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+  `mongodb+srv://himanshu:RNKTyKW2kQNJnxq2&&@cluster0.q1sgt3q.mongodb.net/shop`;
 
 const app = express();
 const store = new MongoDBStore({
